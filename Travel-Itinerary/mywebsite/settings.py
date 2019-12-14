c03e1729f2a201364fc50fca721022a6775d0e4a
yasinderu <m.yasin449@gmail.com>
2019-12-14 17:18:27 +0700

    akshd

--- Travel-Itinerary/mywebsite/settings.py
+++ Travel-Itinerary/mywebsite/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'skripsi',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
