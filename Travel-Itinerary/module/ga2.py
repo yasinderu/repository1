68c8bc30f1bc6501742256f60ff64b9c84e00a9c
yasinderu <m.yasin449@gmail.com>
2019-12-14 18:06:07 +0700

    akshd

--- Travel-Itinerary/module/ga2.py
+++ Travel-Itinerary/module/ga2.py
@@ -39,36 +39,6 @@ class ga():
 		self.distMatrice = pd.DataFrame(self.distArr, index=self.population, columns=self.population)
 		self.durMatrice = pd.DataFrame(self.durArr, index=self.population, columns=self.population)
 
-	def geneticAlgorithm(self):
-		start_time = time.time()
-		lat = []
-		lng = []
-		place_id = []
-		place_url = []
-		population = self.population[1:len(self.population)]
-		pops = self.initialPopulation(population)
-		for i in range(0, self.generations):
-			pops = self.nextGeneration(pops)
-		final_distance = float(1/self.rankRoutes(pops)[0][1]) / 1000
-		bestRouteIndex = self.rankRoutes(pops)[0][0]
-		bestRoute = pops[bestRouteIndex]
-
-		for item in bestRoute:
-			loc = gmaps.geocode(item)
-			lat.append(loc[0]['geometry']['location']['lat'])
-			lng.append(loc[0]['geometry']['location']['lng'])
-			place_id.append(loc[0]['place_id'])
-
-		duration = self.getDuration(bestRoute)
-		detail = list(zip(bestRoute, lat, lng, place_id))
-
-		result = {'routes': bestRoute, 'addr':detail, 'lat': lat, 'lng': lng, 'distance': int(final_distance), 'duration': duration}
-		end_time = time.time()
-		print("Best route : " + str(bestRoute))
-		print("Distance : " + str(final_distance))
-		print("Running Time : " + str(end_time - start_time))
-		return result
-
 	# def join(self, *args):
 	# 	threading.Thread.join(self, *args)
 	# 	return self._return
@@ -86,9 +56,11 @@ class ga():
 				toCity = route[i + 1]
 			else:
 				toCity = route[i]
-			duration += self.durMatrice[fromCity][toCity]
-		if duration/3600 > 1:
-			totalDuration = int(duration/3600), 'Jam', int(duration%3600)/60, 'Menit'
+			duration += self.durMatrice[fromCity][toCity] + 7200
+			print(duration)
+			# durr = duration + 120
+		if int(duration/3600) > 1:
+			totalDuration = int(duration/3600), 'Jam', int(duration%3600/60), 'Menit'
 		else:
 			totalDuration = int(duration/60), 'Menit'
 		return totalDuration
@@ -223,3 +195,44 @@ class ga():
 		children = self.breedPopulation(matingpool)
 		nextGeneration = self.mutatePopulation(children)
 		return nextGeneration
+
+	def geneticAlgorithm(self):
+		start_time = time.time()
+		lat = []
+		lng = []
+		place_id = []
+		place_url = []
+		population = self.population[1:len(self.population)]
+		pops = self.initialPopulation(population)
+		for i in range(0, self.generations):
+			pops = self.nextGeneration(pops)
+		final_distance = float(1/self.rankRoutes(pops)[0][1]) / 1000
+		bestRouteIndex = self.rankRoutes(pops)[0][0]
+		bestRoute = pops[bestRouteIndex]
+
+		for item in bestRoute:
+			loc = gmaps.geocode(item)
+			lat.append(loc[0]['geometry']['location']['lat'])
+			lng.append(loc[0]['geometry']['location']['lng'])
+			place_id.append(loc[0]['place_id'])
+
+		duration = self.getDuration(bestRoute)
+		detail = list(zip(bestRoute, lat, lng, place_id))
+
+		dist = []
+		for i in range(0, len(bestRoute)-1):
+			fromCity = bestRoute[i]
+			toCity = None
+			if i + 1 < len(bestRoute):
+				toCity = bestRoute[i + 1]
+			else:
+				toCity = bestRoute[i]
+			dist.append(self.getDistance(fromCity, toCity))
+
+		result = {'routes': bestRoute, 'dist': dist, 'addr':detail, 'lat': lat, 'lng': lng, 'distance': int(final_distance), 'duration': duration}
+		end_time = time.time()
+		print("Best route : " + str(bestRoute))
+		print("Distance : " + str(final_distance))
+		print("Running Time : " + str(end_time - start_time))
+		print(result)
+		return result
